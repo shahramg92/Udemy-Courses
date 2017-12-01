e21cecbb69ed297a3ca474a90071c46a00b2c613
Shahram Ghassemi <shahramg92@hotmail.com>
2017-12-01 12:03:05 -0600

    update

--- React16/assignment2/src/App.js
+++ React16/assignment2/src/App.js
@@ -13,7 +13,6 @@ class App extends Component {
           <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
           <li>When you click a CharComponent, it should be removed from the entered text.</li>
         </ol>
-        <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
       </div>
     );
   }

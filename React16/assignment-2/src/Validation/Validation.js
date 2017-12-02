8c47fa212385bc0951ddeada0c21c7ace1cea481
Shahram Ghassemi <shahramg92@hotmail.com>
2017-12-02 00:17:35 -0600

    update

---
+++ React16/assignment-2/src/Validation/Validation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const validation = (props) => {
+  return (
+    <div>
+      <p>
+        The length of the text is {props.inputLength}
+      </p>
+    </div>
+  )
+};
+
+export default validation;

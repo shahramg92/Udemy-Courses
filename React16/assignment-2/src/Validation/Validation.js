e40835f1ef45507a34879a930a214d08cf9b0229
Shahram Ghassemi <shahramg92@hotmail.com>
2017-12-04 15:20:23 -0600

    update

--- React16/assignment-2/src/Validation/Validation.js
+++ React16/assignment-2/src/Validation/Validation.js
@@ -4,9 +4,9 @@ const validation = (props) => {
   return (
     <div>
       {
-
-
-        
+        props.inputLength > 5 ?
+          <p>Text long enough</p> :
+          <p>Text too short!</p>
       }
     </div>
   )

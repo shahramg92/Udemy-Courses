33946f0bd7a6bbfcc0e50f827f0718e63dd8169b
Shahram Ghassemi <shahramg92@hotmail.com>
2017-12-04 16:05:23 -0600

    updates

--- React16/assignment-2/src/Validation/Validation.js
+++ React16/assignment-2/src/Validation/Validation.js
@@ -1,15 +1,27 @@
 import React from 'react';
 
 const validation = (props) => {
+  let validationMessage = 'Text long enough';
+
+  if (props.inputLength <= 5) {
+    validationMessage = 'Text too short';
+  }
+
   return (
     <div>
-      {
-        props.inputLength > 5 ?
-          <p>Text long enough</p> :
-          <p>Text too short!</p>
-      }
+      <p>{validationMessage}</p>
     </div>
   )
 };
+
+//
+// You can also put this in the div and it will work the same:
+// if props.inputLength is greater than five, then write text long enough. otherwise, print out text too short
+// {
+//   props.inputLength > 5 ?
+//     <p>Text long enough</p> :
+//     <p>Text too short!</p>
+// }
+
 
 export default validation;

811031067eff86cce9485eaacc940bd649f5c327
Shahram Ghassemi <shahramg92@hotmail.com>
2018-01-31 16:10:20 -0600

    update

--- React16/Take2/code-along/assignment2/src/Validation/Validation.js
+++ React16/Take2/code-along/assignment2/src/Validation/Validation.js
@@ -3,18 +3,20 @@ import React from 'react';
 const validation = (props) => {
   let validationMessage = 'Text long enough'
 
-  if {props.inputLength} > 5
-
+  if (props.inputLength <= 5) {
+    validationMessage = 'Text too short';
+  }
 
   return (
     <div>
-      {
-        props.inputLength > 5 ?
-          <p>Text long enough</p> :
-          <p>Text too short!</p>
-      }
+      <p>{validationMessage}</p>
     </div>
 )};
 
+// {/* {
+//   props.inputLength > 5 ?
+//     <p>Text long enough</p> :
+//     <p>Text too short!</p>
+// // } */} you could also use a tertiary expression but it is recommended to use the above method
 
 export default validation;

7194d2c86b947b7f59a2ba4360260a45389768fd
Shahram Ghassemi <shahramg92@hotmail.com>
2018-01-30 22:56:10 -0600

    update

---
+++ React16/Take2/code-along/assignment2/src/Validation/Validation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const validation = (props) => {
+  let validationMessage = 'Text long enough'
+
+  if {props.inputLength} > 5
+
+
+  return (
+    <div>
+      {
+        props.inputLength > 5 ?
+          <p>Text long enough</p> :
+          <p>Text too short!</p>
+      }
+    </div>
+)};
+
+
+export default validation;

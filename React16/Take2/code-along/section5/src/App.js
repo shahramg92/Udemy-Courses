e2ccede99010abe19b42e7cbf8c1647c0613187a
Shahram Ghassemi <shahramg92@hotmail.com>
2018-01-31 18:33:07 -0600

    update

--- React16/Take2/code-along/section5/src/App.js
+++ React16/Take2/code-along/section5/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import Radium from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -51,7 +52,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover':{
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -70,15 +75,29 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
     }
 
-    let classes = ['red', 'bold'].join(' ');
+    // let classes = ['red', 'bold'].join(' ');
     // this will turn the array of strings into one string "red bold"
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red');
+      // classes will be red
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push('bold');
+      // now, classes = ['red', 'bold']
+    }
 
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button
           style={style}
           onClick={this.togglePersonsHandler}>Toggle Persons</button>
@@ -89,4 +108,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);

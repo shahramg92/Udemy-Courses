bbc7f3ccc3c23d029cdc9ce69846e6ddbaff0efd
Shahram Ghassemi <shahramg92@hotmail.com>
2018-02-01 09:12:15 -0600

    update

--- React16/Take2/code-along/section5/config/webpack.config.dev.js
+++ React16/Take2/code-along/section5/config/webpack.config.dev.js
@@ -167,7 +167,7 @@ module.exports = {
                 options: {
                   importLoaders: 1,
                   modules: true,
-                  localIdentName: '[name__[local]__[hash:base64:5]]'
+                  localIdentName: '[name]__[local]__[hash:base64:5]'
                 },
               },
               {

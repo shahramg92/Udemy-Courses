903b9e90a141932cdcfaa37c9cb0140068004c11
Shahram Ghassemi <shahramg92@hotmail.com>
2018-02-07 17:37:55 -0600

    update

--- React16/Modules/code/section7-deeper-react/src/components/Persons/Person/Person.js
+++ React16/Modules/code/section7-deeper-react/src/components/Persons/Person/Person.js
@@ -3,7 +3,21 @@ import React, {Component} from 'react';
 import classes from './Person.css';
 
 class Person extends Component {
+  constructor(props) {
+    super(props)
+    console.log('[Persons.js] Inside Constructor', props);
+  }
+
+  componentWillMount() {
+    console.log('[Persons.js] Inside componentWillMount()');
+  }
+
+  componentDidMount() {
+    console.log('[Persons.js] Inside componentDidMount');
+  }
+
   render () {
+    console.log('[Persons.js] Inside render');
     return (
         <div className={classes.Person}>
             <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old!</p>

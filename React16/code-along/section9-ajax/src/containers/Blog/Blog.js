cdd81e2e8d4b0be60531e38ed244b2e8f4e0c558
Shahram Ghassemi <shahramg92@hotmail.com>
2018-02-26 17:16:35 -0600

    update

--- React16/code-along/section9-ajax/src/containers/Blog/Blog.js
+++ React16/code-along/section9-ajax/src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   }
 
   componentDidMount () {
@@ -26,11 +27,16 @@ class Blog extends Component {
       });
   }
 
+  postSelectedHandler = (id) => {
+    this.setState({selectedPostId: id})
+  }
+
     render () {
       const posts = this.state.posts.map(post => {
         return <Post key={post.id}
                     title={post.title}
-                    author={post.author}/>
+                    author={post.author}
+                    clicked={() => this.postSelectedHandler(post.id)}/>
       });
 
         return (
@@ -39,7 +45,7 @@ class Blog extends Component {
                   {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />

87ba8f333d484b71746cba1ceef42e659961ca45
Shahram Ghassemi <shahramg92@hotmail.com>
2018-03-05 17:27:36 -0600

    update

--- React16/code-along/section11-routing/src/containers/Blog/Blog.js
+++ React16/code-along/section11-routing/src/containers/Blog/Blog.js
@@ -19,6 +19,7 @@ class Blog extends Component {
                 </nav>
               </header>
               <Route path="/" exact render={() => <h1>Home</h1>} />
+              <Route path ="/" exact component={Posts} />
             </div>
         );
     }

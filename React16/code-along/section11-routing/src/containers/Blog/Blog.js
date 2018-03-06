4edaa56202de842eaf831227185b205d6d952446
Shahram Ghassemi <shahramg92@hotmail.com>
2018-03-05 23:18:12 -0600

    updates

--- React16/code-along/section11-routing/src/containers/Blog/Blog.js
+++ React16/code-along/section11-routing/src/containers/Blog/Blog.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import Post from './FullPost/FullPost';
 
 class Blog extends Component {
 
@@ -14,18 +15,26 @@ class Blog extends Component {
               <header>
                 <nav>
                   <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to={{
+                    <li><NavLink
+                      to="/"
+                      exact
+                      activeClassName="my-active"
+                      activeStyle={{
+                        color: '#fa923f',
+                        textDecoration:'underline'
+                      }}>Home</NavLink></li>
+                    <li><NavLink to={{
                       pathname: '/new-post',
                       hash: '#submit',
                       search: '?quick-submit=true'
-                    }}>New Post</Link></li>
+                    }}>New Post</NavLink></li>
                   </ul>
                 </nav>
               </header>
               {/* <Route path="/" exact render={() => <h1>Home</h1>} /> */}
               <Route path ="/" exact component={Posts} />
               <Route path="/new-post" component={NewPost} />
+              <Route path ="/:id" exact component={FullPost} />
             </div>
         );
     }

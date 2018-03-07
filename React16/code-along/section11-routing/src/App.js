2638c9a2c979f5b4095116f5d8e0e6fa39dda0ed
Shahram Ghassemi <shahramg92@hotmail.com>
2018-03-06 22:06:50 -0600

    updates

--- React16/code-along/section11-routing/src/App.js
+++ React16/code-along/section11-routing/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <div lassName="App">
+      <div className="App">
         <Blog />
       </div>
     </BrowserRouter>

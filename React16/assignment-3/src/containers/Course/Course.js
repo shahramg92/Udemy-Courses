f88876371f1c4facf14ccd0e3690012ad41413c1
Shahram Ghassemi <shahramg92@hotmail.com>
2018-03-21 16:02:07 -0500

    update

--- React16/assignment-3/src/containers/Course/Course.js
+++ React16/assignment-3/src/containers/Course/Course.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
-  state = {
-    courseTitle: ''
-  }
-
+    state = {
+        courseTitle: ''
+    }
+    
     componentDidMount () {
-      this.parseQueryParams();
+        this.parseQueryParams();
     }
 
-    componentDidUpdate () {
-      this.parseQueryParams();
+    componentDidUpdate() {
+        this.parseQueryParams();
     }
 
     parseQueryParams () {
-      console.log(this.props);
-      const query = new URLSearchParams(this.props.location.search);
-      for (let param of query.entries()) {
-        this.setState({courseTitle: param[1]});
-      }
+        console.log(this.props);
+        const query = new URLSearchParams(this.props.location.search);
+        for (let param of query.entries()) {
+            if (this.state.courseTitle !== param[1]) {
+                this.setState({courseTitle: param[1]});
+            }
+        }
     }
 
     render () {
--- React16/assignment-3/src/containers/Courses/Courses.js
+++ React16/assignment-3/src/containers/Courses/Courses.js
@@ -21,13 +21,14 @@ class Courses extends Component {
                     {
                         this.state.courses.map( course => {
                             return (
-                              <Link key={course.id}
+                                <Link 
+                                    key={course.id} 
                                     to={{
-                                      pathname: this.props.match.url + '/' + course.id,
-                                      search: '?title=' + course.title
+                                        pathname: this.props.match.url + '/' + course.id,
+                                        search: '?title=' + course.title
                                     }}>
-                                <article className="Course">{course.title}</article>
-                              </Link>
+                                    <article className="Course">{course.title}</article>
+                                </Link>
                             );
                         } )
                     }

39a3049766008af92bb201b07215cf9274964ad8
Shahram Ghassemi <shahramg92@hotmail.com>
2018-03-28 00:21:54 -0500

    update

--- React16/code-along/section14-redux/src/containers/Counter/Counter.js
+++ React16/code-along/section14-redux/src/containers/Counter/Counter.js
@@ -15,7 +15,7 @@ class Counter extends Component {
                 <CounterControl label="Add 10" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 15" clicked={this.props.onSubtractCounter}  />
                 <hr />
-                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
                   {this.props.storedResults.map(strResult => (
                     <li
@@ -30,8 +30,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
   return {
-    ctr : state.counter,
-    storedResults: state.results
+    ctr : state.ctr.counter,
+    storedResults: state.res.results
   };
 };
 
@@ -41,7 +41,7 @@ const mapDispatchToProps = dispatch => {
     onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
     onAddCounter: () => dispatch({type:'ADD', value: 10}),
     onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, value: 15}),
-    onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT}),
+    onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result: result}),
     onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElementId: id})
   };
 };
--- React16/code-along/section14-redux/src/store/reducers/counter.js
+++ React16/code-along/section14-redux/src/store/reducers/counter.js
@@ -1,8 +1,7 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 
 const initialState = {
   counter: 0,
-  results: []
 }
 
 const reducer = (state = initialState, action) => {
@@ -31,15 +30,6 @@ const reducer = (state = initialState, action) => {
         ...state,
         results:state.results.concat({id: new Date(), value: state.counter})
       }
-    case actionTypes.DELETE_RESULT:
-    // const id = 2;
-    // const newArray = [...state.results]
-    // newArray.splice(id, 1)
-    const updatedArray = state.results.filter(result => result.id !== action.resultElementId);
-    return {
-      ...state,
-      results: updatedArray
-    }
   }
   return state;
 };

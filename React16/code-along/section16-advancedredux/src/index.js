052ba8920f5baebedf3d05db64481f7831052ae8
Shahram Ghassemi <shahramg92@hotmail.com>
2018-03-31 15:54:30 -0500

    middlware

--- React16/code-along/section16-advancedredux/src/index.js
+++ React16/code-along/section16-advancedredux/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
@@ -18,12 +18,16 @@ const logger = store => {
   return next => {
     return action => {
       console.log('[Middleware] Dispatching', action);
-      next(action);
+      const result = next(action);
+      console.log('[Middleware] next state', store.getState());
+      return result;
     }
   }
 };
 
-const store = createStore(rootReducer);
+const composeEnhacers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhacers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();

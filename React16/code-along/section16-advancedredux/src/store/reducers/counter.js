89d0cd83cad4ea191e71e136199db4b896b82a7e
Shahram Ghassemi <shahramg92@hotmail.com>
2018-04-02 19:02:33 -0500

    updates

--- React16/code-along/section16-advancedredux/src/store/reducers/counter.js
+++ React16/code-along/section16-advancedredux/src/store/reducers/counter.js
@@ -8,25 +8,13 @@ const initialState = {
 const reducer = ( state = initialState, action ) => {
     switch ( action.type ) {
         case actionTypes.INCREMENT:
-            const newState = Object.assign({}, state);
-            newState.counter = state.counter + 1;
-            return newState;
+            return updateObject(state, {counter: state.counter + 1});
         case actionTypes.DECREMENT:
-            return {
-                ...state,
-                counter: state.counter - 1
-            }
+            return updateObject(state, {counter: state.counter - 1 });
         case actionTypes.ADD:
-            return {
-                ...state,
-                counter: state.counter + action.val
-            }
+            return updateObject(state, {counter: state.counter + action.val });
         case actionTypes.SUBTRACT:
             return updateObject(state, {counter: state.counter - action.val});
-            return {
-                ...state,
-                counter: state.counter - action.val
-            }
     }
     return state;
 };

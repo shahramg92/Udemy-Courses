28fe23eaad280b0075ffed7313c5348a94d37adb
Shahram Ghassemi <shahramg92@hotmail.com>
2019-01-02 13:11:03 -0600

    update

---
+++ Web-dev-camp/Javascript/Objects/objects1.js
@@ -0,0 +1,30 @@
+var movies = [
+    { 
+      title: "In Bruges",
+      rating:  3,
+      hasWatched: "No"
+    },
+    { 
+      title: "Frozen",
+      rating: 4,
+      hasWatched: "Yes"
+    },
+    { 
+      title: "Mad Max Fury Road",
+      rating:  5,
+      hasWatched: "No"
+    },
+    { 
+      title: "Les Miserables",
+      rating:  4,
+      hasWatched: "No"
+    }
+];
+
+movies.forEach(function(obj) {
+    if (obj.hasWatched === "Yes") {
+        console.log("You have watched: " + obj.title)
+    } else {
+        console.log("You have not watched: " + obj.title)
+    }
+});

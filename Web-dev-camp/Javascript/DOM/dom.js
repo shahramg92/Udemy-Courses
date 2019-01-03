0032180afde6c556b5fb40826b1fddc6d22deb6a
Shahram Ghassemi <shahramg92@hotmail.com>
2019-01-03 11:27:31 -0600

    update

---
+++ Web-dev-camp/Javascript/DOM/dom.js
@@ -0,0 +1,10 @@
+//exercise 1 5 ways to select p1
+document.getElementById("first")
+
+document.getElementsByClassName(".special")
+
+document.getElementsByTagName("p")
+
+document.getElementsByName("#first")
+
+document.querySelector("p")

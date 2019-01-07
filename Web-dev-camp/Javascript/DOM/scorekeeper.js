6ab24d585eacff32dc699598c63044b30fb4cb68
Shahram Ghassemi <shahramg92@hotmail.com>
2019-01-07 15:50:54 -0600

    update

--- Web-dev-camp/Javascript/DOM/scorekeeper.js
+++ Web-dev-camp/Javascript/DOM/scorekeeper.js
@@ -47,7 +47,7 @@ function reset() {
 };
 
 numInput.addEventListener("change", function() {
-    winningScoreDisplay.textContent = numInput.value;
-    winningScore = Number(numInput.value);
+    winningScoreDisplay.textContent = this.value;
+    winningScore = Number(this.value);
     reset();
 });

42541b6f4a6e7f8dae8db5f29cefff70de40e14f
Shahram Ghassemi <shahramg92@hotmail.com>
2019-01-07 11:56:31 -0600

    update

--- Web-dev-camp/Javascript/DOM/colorchanger.js
+++ Web-dev-camp/Javascript/DOM/colorchanger.js
@@ -1,12 +1,20 @@
 var buttton = document.querySelector("button");
-var isPurple = false;
+
+
+// var isPurple = false;
+
+// buttton.addEventListener("click", function() {
+//   if(isPurple) {
+//       document.body.style.backgroundColor = "white";
+//       isPurple = false;
+//   } else {
+//       document.body.style.backgroundColor = "purple";
+//       isPurple = true;
+//   }
+// });
+
+//or shorter
 
 buttton.addEventListener("click", function() {
-  if(isPurple) {
-      document.body.style.backgroundColor = "white";
-      isPurple = false;
-  } else {
-      document.body.style.backgroundColor = "purple";
-      isPurple = true;
-  }
-});
+    document.body.classList.toggle("purple");
+  });
